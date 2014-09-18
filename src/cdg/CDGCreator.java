7d53e84bdf683008bc70c6df6009425499b291ad
Alwin Maier <alwin.maier@stud.uni-goettingen.de>
2014-09-18 14:45:56 +0200

    Remove class ReverseCFG.
    Use the reverse-function to reverse the control flow graph.

--- src/cdg/CDGCreator.java
+++ src/cdg/CDGCreator.java
@@ -1,5 +1,7 @@
 package cdg;
 
+import graphutils.Edge;
+import graphutils.IncidenceListGraph;
 import cfg.CFG;
 import cfg.nodes.CFGNode;
 import dom.DominatorTree;
@@ -9,11 +11,12 @@ public class CDGCreator
 
 	public CDG create(CFG cfg)
 	{
-		ReverseCFG reverseCFG;
-		DominatorTree<CFGNode> dominatorTree;
-		reverseCFG = ReverseCFG.newInstance(cfg);
-		dominatorTree = DominatorTree.newInstance(reverseCFG,
-				reverseCFG.getEntryNode());
+		IncidenceListGraph<CFGNode, Edge<CFGNode>> reverseCfg = cfg.reverse();
+		Edge<CFGNode> augment = new Edge<CFGNode>(cfg.getExitNode(),
+				cfg.getEntryNode());
+		reverseCfg.addEdge(augment);
+		DominatorTree<CFGNode> dominatorTree = DominatorTree.newInstance(
+				reverseCfg, cfg.getExitNode());
 		return CDG.newInstance(dominatorTree);
 	}
 }
