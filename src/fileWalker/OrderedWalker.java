114b93ac964ce503195043276b24e297e665629e
ideadapt <kunz@ideadapt.net>
2015-04-27 22:28:24 +0200

    fix orderedwalker to always report relative file paths.

--- src/fileWalker/OrderedWalker.java
+++ src/fileWalker/OrderedWalker.java
@@ -3,18 +3,23 @@ package fileWalker;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OrderedWalker extends SourceFileWalker
 {
 	FileNameMatcher matcher = new FileNameMatcher();
-	List<SourceFileListener> listeners = new LinkedList<SourceFileListener>();
+	List<SourceFileListener> listeners = new LinkedList<>();
+	private final String BASE_PATH = Paths.get("").toAbsolutePath().toString()+"/";
 	
-	public OrderedWalker()
+	public OrderedWalker(String[] extensions)
 	{
-		setFilenameFilter(DEFAULT_FILENAME_FILTER);
+		String extensionList = Arrays.asList(extensions).stream().collect(Collectors.joining(","));
+		String pattern = "*.{" + extensionList + "}";
+		setFilenameFilter(pattern);
 	}	
 	
 	@Override
@@ -30,8 +35,7 @@ public class OrderedWalker extends SourceFileWalker
 	}
 
 	@Override
-	protected void walkExistingFileOrDirectory(String dirOrFileName)
-			throws IOException
+	protected void walkExistingFileOrDirectory(String dirOrFileName) throws IOException
 	{
 		walk(dirOrFileName);
 	}
@@ -42,24 +46,24 @@ public class OrderedWalker extends SourceFileWalker
 		File[] dirContent = file.listFiles();
 		Path path = file.toPath();
 		
-		
-		// if this is not a directory
+		// if this is not a directory, or invalid path
 		if(dirContent == null) return;
 		Arrays.sort(dirContent);
 		
 		reportDirectoryEnter(path);
 		
 		for(File f : dirContent){
-			Path filePath = f.toPath();
 			String absolutePath = f.getAbsolutePath();
-			
+
 			if(f.isDirectory()){	
 				walk(absolutePath);
 				continue;
 			}
-			
-			if(matcher.fileMatches(filePath))
-				reportFile(filePath);
+
+			Path filePath = f.toPath();
+			Path relativePath = Paths.get(filePath.toString().replace(BASE_PATH, ""));
+			if(matcher.fileMatches(relativePath))
+				reportFile(relativePath);
 		}
 	
 		reportDirectoryLeave(path);
@@ -83,6 +87,4 @@ public class OrderedWalker extends SourceFileWalker
 			listener.visitFile(path);
 
 	}
-
-	
 }
--- src/tests/CodebaseWalkerTest.java
+++ src/tests/CodebaseWalkerTest.java
@@ -1,45 +1,40 @@
 package tests;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
+import fileWalker.OrderedWalker;
 import org.junit.Test;
 
 import fileWalker.SourceFileWalker;
-import fileWalker.UnorderedWalker;
 
 public class CodebaseWalkerTest
 {
-
 	@Test
 	public void testRecursiveDirSearch()
 	{
 		String[] args = { "src/tests/samples/" };
 
-		SourceFileWalker provider = new UnorderedWalker();
+		SourceFileWalker provider = new OrderedWalker(new String[]{"cpp", "c"});
 
-		try
-		{
+		try {
 			Set<String> expected = new HashSet<String>();
+			expected.add("src/tests/samples/tiff.cpp");
 			expected.add("src/tests/samples/subdir/test.c");
 			expected.add("src/tests/samples/test.c");
-			expected.add("src/tests/samples/tiff.cpp");
 
 			FilenameAggregator listener = new FilenameAggregator();
 			provider.addListener(listener);
 			provider.walk(args);
 
-			assertEquals(expected, new HashSet<String>(listener.filenames));
+			assertEquals(expected, new HashSet<>(listener.filenames));
 		}
-		catch (IOException e)
-		{
+		catch (IOException e) {
 			fail("IO Error");
 		}
 	}
-
 }
